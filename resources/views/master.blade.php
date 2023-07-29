0bfac84bea785112cf3f397c493e81c42b088804
Thanos Giapoutzis <thanosgiap@gmail.com>
2023-07-29 20:25:19 +0300

    Configure vite , add bootstrap

---
+++ resources/views/master.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Erodios Restaurant</title>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&family=Roboto:wght@100&family=Water+Brush&display=swap" rel="stylesheet">
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    
+</head>
+<body>
+    <header>
+        <!-- Your header content -->
+    </header>
+
+    <nav>
+        <!-- Your navigation menu -->
+    </nav>
+
+    <main>
+        <!-- Content from other views will be inserted here -->
+        @yield('content')
+    </main>
+
+    <footer>
+        <!-- Your footer content -->
+    </footer>
+
+    <!-- Add your common JavaScript files here -->
+</body>
+</html>
